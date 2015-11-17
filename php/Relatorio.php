93051b368131b63f990d0737cf1a395e9e7b819d
Elton Custodio Ribeiro <elton_custodio@live.com>
2015-11-17 09:41:08 +0000

    Busca risco

--- php/Relatorio.php
+++ php/Relatorio.php
@@ -37,3 +37,14 @@ $table['rows'] = $rows;
 // encode the table as JSON
 $jsonTable = json_encode($table);
 echo $jsonTable;
+
+//Consultando banco de dados
+    $qryLista = mysqli_query($con, "SELECT Pessoas from cliente where risco >50 ");    
+    while($resultado = mysqli_fetch_assoc($qryLista)){
+        $vetor[] = array_map('utf8_encode', $resultado); 
+    }    
+    
+    //Passando vetor em forma de json
+    echo json_encode($vetor);
+    
+?>
