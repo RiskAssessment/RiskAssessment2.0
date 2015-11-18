e22ac5049c465d0d6709551bebb74de1f1de9f40
dairel <dairelsi@live.com>
2015-11-18 17:54:11 +0000

    Botão delete

--- js/ajax.js
+++ js/ajax.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
-	$.ajax({
-		type:'post',		//Definimos o m�todo HTTP usado
+	        $('#tabela').empty(); //Limpando a tabela
+                $.ajax({
+		type:'POST',		//Definimos o mï¿½todo HTTP usado
 		dataType: 'json',	//Definimos o tipo de retorno
-		url: 'getdados.php',//Definindo o arquivo onde ser�o buscados os dados
+		url: 'getDados.php',//Definindo o arquivo onde serï¿½o buscados os dados
+
 		success: function(dados){
 			for(var i=0;dados.length>i;i++){
 				//Adicionando registros retornados na tabela
-				$('#tabela').append('<tr><td>'+dados[i].id+'</td><td>'+dados[i].nome+'</td><td>'
-				 +dados[i].cpf+'</td><td>'+dados[i].empresa+'</td><td>'+dados[i].risco+'</td></tr>');
+				$('#tabela').append('<tr><td>'+dados[i].Id+'</td><td>'+dados[i].nome+'</td><td>'
+				 +dados[i].cpf+'</td><td>'+dados[i].empresa+'</td><td>'+dados[i].risco+'</td> <td><p><input type="button" value="Deletar" /></p></td></tr>');
 			}
 		}
 	});
